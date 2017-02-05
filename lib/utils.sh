7014d049e042340b544a68a8a08667679d8a2fcb
Marcos Hernández <marcoslhc@gmail.com>
2017-02-05 16:50:22 -0500

    Fix templates because bash args sucks

--- lib/utils.sh
+++ lib/utils.sh
@@ -9,29 +9,19 @@ render_template() {
 			#         |__________________________ 1 parameter in the first position
 			#                                     from the tail
 
-	local substitutions="${@: 1: ${#}-2}"       # get all the rest
-							# |    |  |__________ counting 2 position
-							# |    |_____________ from the array length
-							# |__________________ take all the elements from the
-							#                     first position
-
 	local context=()                            # initialize context
 
 	# create context
 	# context variables comes in the form VARIABLENAME=value
 
-	for subst in "${substitutions}";do
+	for subst in "${@}";do
 		if [[ "${subst}" =~ ^.*=.*$ ]]; then
-			IFS='=' read -a ctxvar <<< $subst;  # string splitting, this is cool
-			#    |  |        |          |________ we feed the function with the args and
-			#    |  |        |___________________ we store in this variable the values
-			#    |  |____________________________ we read here
-			#    |_______________________________ IFS is the Internal Field Separator
-			tplstr="${ctxvar[0]}"; valstr="${ctxvar[1]}"
+			IFS='=' read -a ctxvar <<< $subst; tplstr="${ctxvar[0]}"; valstr="${ctxvar[1]}"
 			context+=('-e '"s/\{\{${tplstr}\}\}/$valstr/g")
+			info:sm "Setting ${tplstr} to ${valstr}"
 		fi
 	done
 
 	#execute the substitutions
-	sed -E "${context[@]} ${template} > ${dest}"
+	sed -E "${context[@]}" $template > $dest
 }
