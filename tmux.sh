324685e9f4e78723b7745d12f0bdf962e7d525d8
Marcos Hernández <marcoslhc@gmail.com>
2017-03-05 22:25:22 -0500

    Add reattach-to-user-namespace for better copypaste

--- tmux.sh
+++ tmux.sh
@@ -5,14 +5,14 @@ MAIN_WINDOW_SIZE=$(tput cols)
 PROJECTS_DIR="${HOME}/Documents/projects"
 
 if [[ -z $TMUX ]]; then
-    tmux -2 new-session -Ad -s $SESSION
-    tmux split-window -h -t $SESSION:1.0
-    tmux split-window -v -t $SESSION:1.1
-    tmux split-window -v -t $SESSION:1.2
-    tmux select-layout -t $SESSION:1 main-vertical
+    tmux -2 new-session -Ad -s $SESSION                      # Opens the session with window 1
+    tmux split-window -h -t $SESSION:1.0                     # Split the window horizontally
+    tmux split-window -v -t $SESSION:1.1                     # Split the upper panel vertically
+    tmux split-window -v -t $SESSION:1.2                     # Split the lower panel vertically
+    tmux select-layout -t $SESSION:1 main-vertical           # Select special layout with one main panel to the left
     tmux send-keys -t $SESSION:1.0 "cd ${PROJECTS_DIR}" C-m
     tmux send-keys -t $SESSION:1.1 "cd ${PROJECTS_DIR}" C-m
     tmux send-keys -t $SESSION:1.2 "cd ${PROJECTS_DIR}" C-m
     tmux send-keys -t $SESSION:1.3 "cd ${PROJECTS_DIR}" C-m
-    tmux attach -t $SESSION
+    tmux attach -t $SESSION                                  # Attach the session
 fi
